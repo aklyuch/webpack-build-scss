491da0713f26e01bb9db593da129655d32b8d696
aklyuch <toheen@gmail.com>
2021-05-15 10:18:37 +0300

    base commit

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = env => ({
+    entry: './src/app.js',
+    mode: 'production',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 8080,
+        index: 'index.html'
+    },
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: env.production ? MiniCssExtractPlugin.loader : "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+
+                    ]
+            },
+            {
+                test: /\.hbs/,
+                use: 'handlebars-loader'
+            },
+            {
+                test: /\.(svg|gif|png|eot|woff|ttf)$/,
+                include: path.resolve(__dirname, 'img'),
+                use: [
+                    'url-loader',
+                ],
+            }
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+        }),
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.hbs'),
+            title: 'ВП1 Геоотзыв'
+        })
+    ],
+});
